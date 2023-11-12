8bc6544cbe48de9e18728792053010979ae51e2b
tinawang-jy <43075332+tinawang-jy@users.noreply.github.com>
2023-11-11 20:17:11 -0800

    Update aggregation.py

--- aggregation.py
+++ aggregation.py
@@ -1,29 +1,29 @@
 class Aggregator:
+    @staticmethod
     def count(self, data):
-        """Count the number of elements in the list."""
         count = 0
         for _ in data:
             count += 1
         return count
-
+    
+    @staticmethod
     def sum(self, data):
-        """Calculate the sum of the elements, assuming they can be converted to numbers."""
         total = 0
         for item in data:
             total += float(item)
         return total
-
+    
+    @staticmethod
     def mean(self, data):
-        """Calculate the mean of the elements."""
         total = 0
         count = 0
         for item in data:
             total += float(item)
             count += 1
         return total / count if count else 0
-
+    
+    @staticmethod
     def min(self, data):
-        """Find the minimum value among the elements."""
         if not data:
             return 0
         min_value = float(data[0])
@@ -31,9 +31,9 @@ class Aggregator:
             if float(item) < min_value:
                 min_value = float(item)
         return min_value
-
+    
+    @staticmethod
     def max(self, data):
-        """Find the maximum value among the elements."""
         if not data:
             return 0
         max_value = float(data[0])
@@ -41,3 +41,11 @@ class Aggregator:
             if float(item) > max_value:
                 max_value = float(item)
         return max_value
+    
+if __name__ == "__main__":
+    a = Aggregator()
+    print(a.count([1,2,3,15]))
+    print(a.sum([1,2,3,15]))
+    print(a.mean([1,2,3,15]))
+    print(a.min([1,2,3,15]))
+    print(a.max([1,2,3,15]))
