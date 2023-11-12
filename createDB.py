f4dd81c7cf6ef803682523d7fecd11bb1d6f7962
tongxiny <tongxiny@usc.edu>
2023-11-11 16:25:01 -0800

    createDB done

--- createDB.py
+++ createDB.py
@@ -1,87 +1,105 @@
 import json
 import os
-import re
+import shutil
 
 class DatabaseManager:
-    def __init__(self, directory):
-        self.directory = directory
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        self.relational_path = os.path.join(directory, 'Relational')
-        self.nosql_path = os.path.join(directory, 'NoSQL')
-        if not os.path.exists(self.relational_path):
-            os.makedirs(self.relational_path)
-        if not os.path.exists(self.nosql_path):
-            os.makedirs(self.nosql_path)
-        self.databases = self._get_existing_databases()
+    def __init__(self, command_type=None, database_name=None, database_type=None):
+        self.command_type = command_type
+        self.database_name = database_name
+        self.database_type = database_type
+        self.root = "root"
+        if not os.path.exists(self.root):
+            os.makedirs(self.root)
 
-    def _get_existing_databases(self):
-        # This private method retrieves the existing databases in the directory
-        existing_databases = []
-        for db_type in ['Relational', 'NoSQL']:
-            type_path = os.path.join(self.directory, db_type)
-            for root, dirs, files in os.walk(type_path):
-                for dir in dirs:
-                    existing_databases.append(f"{db_type}/{dir}")
-        return existing_databases
+    def create_database(self):
+        if self.command_type == "create_database":
+            database_dir = f"{self.root}/{self.database_name}_{self.database_type}"
+            if os.path.exists(database_dir):
+                print(f'Database {self.database_name} already exists.')
+            else:
+                os.makedirs(database_dir)
+                print(f'Database {self.database_name} has been created.')
 
-    def create_database(self, name, db_type='Relational'):
-        # This method creates a new database directory
-        if db_type not in ['Relational', 'NoSQL']:
-            raise ValueError("Unsupported database type. Please choose 'Relational' or 'NoSQL'.")
-        db_path = os.path.join(self.directory, db_type, name)
-        if os.path.exists(db_path):
-            raise FileExistsError(f"A database with the name '{name}' already exists.")
-        os.makedirs(db_path)
-        self.databases.append(f"{db_type}/{name}")
-        return f"{db_type}/{name}"
+    def show_database(self):
+        if self.command_type == "show_database":
+            print(self.monitor())
 
-    def database_monitor(self, db_name):
-        # This method monitors a specific database and creates metadata in a txt file
-        db_path = None
-        for db in self.databases:
-            if db_name in db:
-                db_path = os.path.join(self.directory, db)
-                break
-        if not db_path:
-            raise FileNotFoundError(f"No database named '{db_name}' found.")
+    def monitor(self):
+        metadata = []
 
-        dir_info = []
-        for root, dirs, files in os.walk(db_path):
-            for d in dirs:
-                subdir_info = {
-                    'directory_name': d,
-                    'num_files': 0,
-                    'attributes': set()
+        # Scan all db in the directory
+        for db_name in os.listdir(self.root):
+            db_path = os.path.join(self.root, db_name)
+            if os.path.isdir(db_path):
+                database_info = {"database_name": db_name,
+                                 "database_path": db_path,
+                                 "tables": []
                 }
-                subdir_path = os.path.join(root, d)
-                for subroot, subdirs, subfiles in os.walk(subdir_path):
-                    for file in subfiles:
-                        subdir_info['num_files'] += 1
-                        attributes = re.findall(r'\w+', file)
-                        subdir_info['attributes'].update(attributes)
-                dir_info.append(subdir_info)
 
-        metadata = {
-            'database_name': db_name,
-            'tables': dir_info
-        }
-        metadata_path = os.path.join(db_path, f"{db_name}_metadata.txt")
-        with open(metadata_path, 'w') as file:
-            json.dump(metadata, file, indent=4)
 
-        return metadata_path
+                # Scan all tables within the table directory
+                for table_name in os.listdir(db_path):
+                    table_path = os.path.join(db_path,table_name)
+                    if os.path.isdir(table_path):
+                        table_info = {
+                            "table_name": table_name,
+                            "table_path": table_path,
+                            "attributes": set(),
+                            "num of chunks": 0
+                        }
+                        for file_name in os.listdir(table_path):
+                            file_path = os.path.join(table_path,file_name)
+                            table_info['num of chunks'] += 1
+                            if file_name.lower().endswith('.json'):
+                                with open(file_path, 'r', encoding='utf-8') as file:
+                                    try:
+                                        data = json.load(file)
+                                        if isinstance(data, list) and data:
+                                            attributes = set(data[0].keys())
+                                            table_info['attributes'] = table_info['attributes'] | attributes
+                                    except json.JSONDecodeError as e:
+                                        print(f"An error occurred while decoding JSON from file {file_name}: {e}")
+                            elif file_path.lower().endswith('.csv'):
+                                with open(file_path, 'r', encoding='utf-8') as file:
+                                    reader = csv.reader(file)
+                                    headers = next(reader, None)
+                                    if headers:
+                                        table_info['attributes'] = table_info['attributes'] | set(headers)
+                        database_info['tables'].append(table_info)
+                metadata.append(database_info)
 
-    def show_existing_databases(self):
-        # This method shows all existing databases
-        return self.databases
+        return metadata
 
-# Example usage
+    def use_database(self):
+        if self.command_type == "use_database":
+            records = self.monitor()
+            db_names = []
+            db_path = ""
+            for record in records:
+                db_names.append(record['database_name'])
+            if f'{self.database_name}_{self.database_type}' not in db_names:
+                print(f'Database {self.database_name} not exist.')
+            else:
+                for record in records:
+                    if record['database_name'] == f'{self.database_name}_{self.database_type}':
+                        db_path = record['database_path']
+                print(f'Using {self.database_name}.')
+                return db_path
+
+    def drop_database(self):
+        if self.command_type == "drop_database":
+            records = self.monitor()
+            db_names = []
+            for record in records:
+                db_names.append(record['database_name'])
+            if f'{self.database_name}_{self.database_type}' not in db_names:
+                print(f'Database {self.database_name} not exist.')
+            else:
+                path_to_delete = f'{self.root}/{self.database_name}_{self.database_type}'
+                shutil.rmtree(path_to_delete)
+                print(f'Database {self.database_name} is dropped.')
+
+
 if __name__ == "__main__":
-    db_manager = DatabaseManager(".")
-    print("Existing databases:", db_manager.show_existing_databases())
-    created_db = db_manager.create_database("test_db", 'Relational')
-    print(f"Database '{created_db}' created.")
-    metadata_path = db_manager.database_monitor("test_db")
-    print("Metadata file created at:", metadata_path)
-    print("Updated database list:", db_manager.show_existing_databases())
+    test = DatabaseManager("drop_database","xyz","relational")
+    test.drop_database()
